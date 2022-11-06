6a7f9c303116da6bfcb38fad1889d6c5668c031e
supriyadin-inoy-99-co <supri170845@gmail.com>
2022-11-06 21:04:08 +0700

    fixed linter

--- src/features/Posts/action.ts
+++ src/features/Posts/action.ts
@@ -1,26 +1,24 @@
-import { POST_UPDATE, USER_UPDATE } from './constant'
+import { POST_UPDATE, USER_UPDATE } from "./constant"
 
 type stateDataType = {
-	user: {
-		id: string
-	}
+  user: {
+    id: string
+  }
 }
-export const setTitle: Function
-= (title: string) => (dispatch: any) => {
-	dispatch({
-		type: POST_UPDATE,
-		payload: {
-			newTitle: title,
-		},
-	})
+export const setTitle: Function = (title: string) => (dispatch: any) => {
+  dispatch({
+    type: POST_UPDATE,
+    payload: {
+      newTitle: title,
+    },
+  })
 }
 
-export const setUser: Function
-= (data: stateDataType) => (dispatch: any) => {
-	dispatch({
-		type: USER_UPDATE,
-		payload: {
-			newUser: data,
-		},
-	})
+export const setUser: Function = (data: stateDataType) => (dispatch: any) => {
+  dispatch({
+    type: USER_UPDATE,
+    payload: {
+      newUser: data,
+    },
+  })
 }
--- src/pages/survey.tsx
+++ src/pages/survey.tsx
@@ -5,7 +5,6 @@ import jwt from "jsonwebtoken"
 import { Provider } from "react-redux"
 import { store } from "app/store"
 import { Breadcrumb, Layout } from "antd"
-import Header from "@components/Utility/HeaderLayout"
 import Footer from "@components/Utility/FooterLayout"
 import Menus from "@components/Utility/Menus"
 import dynamic from "next/dynamic"
@@ -24,7 +23,6 @@ const Survey: NextPage<Props> = (props: Props) => {
   return (
     <Provider store={store}>
       <Layout>
-        <Header />
         <Content style={{ padding: "0 16px" }}>
           <Breadcrumb style={{ margin: "16px 0" }}>
             <Breadcrumb.Item>Home</Breadcrumb.Item>
@@ -45,7 +43,6 @@ const Survey: NextPage<Props> = (props: Props) => {
               <div className="text-center text-xl">
                 <h1>Evaluasi 1</h1>
               </div>
-              <SurveyComponent />
             </Content>
           </Layout>
         </Content>
