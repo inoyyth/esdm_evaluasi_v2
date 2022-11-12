5456d0340c6eb3f73c818937b2e66f131b2e9486
supriyadin-inoy-99-co <supri170845@gmail.com>
2022-11-12 22:16:23 +0700

    update survey style

---
+++ src/components/SurveyComponent.style.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components"
+
+export const Wrapper = styled.div`
+  .sd-footer {
+    justify-content: end;
+  }
+  .noUi-tooltip {
+    position: fixed;
+    white-space: pre-wrap;
+  }
+  .noUi-horizontal .noUi-tooltip {
+    left: 100%;
+    transform: translate(-65%, 0);
+  }
+`
