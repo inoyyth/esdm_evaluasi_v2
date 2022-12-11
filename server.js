665bf4d673ec0608e05d6282a2fb9865b8f27b1a
supriyadin-inoy-99-co <supri170845@gmail.com>
2022-12-11 09:48:03 +0700

    setup docker

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ dotenv.config()
 const dev = process.env.NODE_ENV !== "production"
 const hostname = "localhost"
 const port = process.env.PORT || 3000
+const expose_port = process.env.EXPOSE_PORT || 3000
 // when using middleware `hostname` and `port` must be provided below
 const app = next({ dev, hostname, port })
 const handle = app.getRequestHandler()
@@ -33,6 +34,6 @@ app.prepare().then(() => {
     }
   }).listen(port, (err) => {
     if (err) throw err
-    console.log(`> Ready on http://${hostname}:${port}`)
+    console.log(`> Ready on http://${hostname}:${expose_port}`)
   })
 })
