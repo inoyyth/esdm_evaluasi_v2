04116b77611c201beb26ef6896b429028f673988
supriyadin-inoy-99-co <supri170845@gmail.com>
2023-10-05 09:15:45 +0700

    update style

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,7 @@ module.exports = {
           "url('https://res.cloudinary.com/abbifam-com/image/upload/v1692119792/ESDM/bpsdm-banner-min_ep8zoe.png')",
         lightYellow:
           "linear-gradient(90deg, rgba(255, 255, 255, 1) 0%, rgba(255, 229, 0, 1) 35%, rgba(255, 229, 0, 1) 100%)",
+        bgGlobal: "url('/images/logo-esdm.png')",
       },
     },
   },
